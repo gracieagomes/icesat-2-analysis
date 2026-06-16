e636541bfda50376f6efde1b85375d1ea99fb951
Gracie Gomes <gagomes@astral.sciclone.wm.edu>
2026-06-16 11:54:30 -0400

    Modify 2: Added descriptions of the script I will be adding to throughout this project. Added the files themselves.

---
+++ script/icesat-2-analysis-script.py
@@ -0,0 +1,11 @@
+# Imported libraries for this project (according to PhoREAL)
+import numpy as py
+import pandas as pd
+import matplotlib.pyplot as plt
+import h5py 
+import scipy
+import rasterio
+import fiona
+import simplekml
+import laspy
+import pyproj
